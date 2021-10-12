6ac804d2c121ed805747bb1395d8ea26a76818cc
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-10-12 20:38:54 -0300

    feat: create jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './jwt.constants';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: { username: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: { username: payload.username },
+    });
+
+    return user;
+  }
+}
