be8e43dddc5ad04e0d0793d06cbc7e3f024712ec
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-10-12 20:36:50 -0300

    feat: add login route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthResponseDto, LoginDto } from './auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  async login(data: LoginDto): Promise<AuthResponseDto> {
+    const { username, password } = data;
+
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('invalid_credentials');
+    }
+
+    return {
+      token: this.jwt.sign({ username }),
+      user: user,
+    };
+  }
+}
