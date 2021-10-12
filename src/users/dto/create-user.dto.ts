75b9b86448bc00d8267a70fc4e8bdcdac94dbc09
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-10-11 21:54:51 -0300

    feat: add validations to dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,36 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(2, 30)
   username: string;
+
+  @IsString()
+  @Length(8, 50)
   password: string;
+
+  @IsString()
+  @Length(2, 30)
   displayName: string;
+
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsUrl()
   img: string;
+
+  @IsString()
+  @Length(2, 280)
   bio: string;
+
+  @IsDateString()
   birth: string;
 }
