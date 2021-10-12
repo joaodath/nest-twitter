1f22d3c9611b678a1e0d1880ea56db1791239545
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-10-11 22:51:44 -0300

    feat: add imports to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from './jwt.constants';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService],
+})
 export class AuthModule {}
